426c5dc7a9bb9e0072d3c828a0766f4c55c7b007
Mladen Petrov <mladen.petrov88@gmail.com>
2019-11-24 13:06:41 +0200

    Add files via upload

---
+++ CharacterMultiplier.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class CharacterMultiplier {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split(" ");
+
+        String first = input[0];
+        String second = input[1];
+
+        int minLength = Math.min(first.length(), second.length());
+        int sum = 0;
+
+        for (int i = 0; i < minLength; i++) {
+           sum += first.charAt(i) * second.charAt(i);
+        }
+
+
+        if (first.length() > second.length()) {
+            for (int i = minLength; i < first.length(); i++) {
+                sum += first.charAt(i);
+            }
+        } else {
+            for (int i = minLength; i < second.length(); i++) {
+                sum += second.charAt(i);
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
